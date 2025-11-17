c2786a6c0c5c290d70a9e6ab8ce62860474fe426
DKhangggg <khangldse183186@fpt.edu.vn>
2025-11-18 00:08:25 +0700

    search input + filter icon

--- frontend/web-client/app/(main)/shop/page.tsx
+++ frontend/web-client/app/(main)/shop/page.tsx
@@ -1,7 +1,26 @@
+import ProductList from "@/components/Product/ProductList";
+import { InputGroupInput } from "@/components/ui/input-group";
+import { FilterIcon, Search } from "lucide-react";
+
 const ShopPage = () => {
   return (
     <div>
-      <div>thanh search</div>
+      <div className="flex items-center justify-start gap-4 p-4">
+        <div className="bg-white border border-amber-800 p-1 rounded-md w-[700px] flex items-center gap-2">
+          <InputGroupInput placeholder="Search..." />
+          <button>
+            <Search />
+          </button>
+        </div>
+        <div className="">
+          <FilterIcon />
+        </div>
+      </div>
+
+      <ProductList />
+      <ProductList />
+      <ProductList />
+      <ProductList />
     </div>
   );
 };
